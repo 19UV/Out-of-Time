5fe901884163fa13f7ac0fa259e0dc394f00c7f1
19UV <the19UV@gmail.com>
2020-07-25 21:23:28 -0400

    Added resources.js

---
+++ build/js/resources.js
@@ -0,0 +1,17 @@
+game.resources = [
+    {
+        "name": "tavern",
+        "type": "image",
+        "src": "data/img/map/tavern.png"
+    },
+    {
+        "name": "tavern_bedroom",
+        "type": "tmx",
+        "src": "data/map/tavern_bedroom.tmx"
+    },
+    {
+        "name": "tavern-bedroom2",
+        "type": "tmx",
+        "src": "data/map/tavern-bedroom2.tmx"
+    }
+];
